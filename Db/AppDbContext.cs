daaf5630a430fb039cf1b17d6d3ab12940d1e2ad
vladisa385 <vladisa375@gmail.com>
2019-06-18 16:11:22 +0700

    наобновалял пакеты для красоты и SOLID

--- Db/AppDbContext.cs
+++ Db/AppDbContext.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using SurviveOnSotka.Entities;
 using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace SurviveOnSotka.Db
 {
